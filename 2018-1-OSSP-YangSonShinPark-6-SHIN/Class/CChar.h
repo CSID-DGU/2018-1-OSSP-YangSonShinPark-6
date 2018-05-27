eaf5a7acd5e8e0153d9934622b388873e93b976a
yeji <ss.yeji1@gmail.com>
2018-05-27 20:51:45 +0900

    CChar.h

--- 2018-1-OSSP-YangSonShinPark-6-SHIN/Class/CChar.h
+++ 2018-1-OSSP-YangSonShinPark-6-SHIN/Class/CChar.h
@@ -18,11 +18,25 @@ class CChar : public CObject {
 	CImageRenderer _Image5;
 	CImageRenderer _Image6;
 
+	int 			 _nSpeed;
+	int 			 _nHp;
+	eItem		 	 _eState;
+
+private:
+	void CheckState();
+
 public:
 	virtual void Init(SDL_Surface * screen);
 	virtual void Update();
 	virtual void Render();
 	virtual void Exit();
+
+	//!< Setter & Getter
+	void SetState(eItem st) { _eState = st; }
+
+	eItem GetState() { return _eState; }
+	int GetSpeed() { return _nSpeed; }
+
 };
 
 #endif /* CLASS_CCHAR_H_ */
