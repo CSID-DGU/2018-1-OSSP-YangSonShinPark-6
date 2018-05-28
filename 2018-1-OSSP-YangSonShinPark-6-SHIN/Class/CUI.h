df9fd85cdbd337f29ea8ccf28c94940241207bda
yeji <ss.yeji1@gmail.com>
2018-05-28 14:27:52 +0900

    bg divide

--- 2018-1-OSSP-YangSonShinPark-6-SHIN/Class/CUI.h
+++ 2018-1-OSSP-YangSonShinPark-6-SHIN/Class/CUI.h
@@ -2,16 +2,22 @@
  * CUI.h
  *
  *  Created on: 2018. 5. 27.
- *      Author: shin-yeji
+ *      Author: root
  */
 
-#ifndef 2018_1_OSSP_YANGSONSHINPARK_6_SHIN_CLASS_CUI_H_
-#define 2018_1_OSSP_YANGSONSHINPARK_6_SHIN_CLASS_CUI_H_
+#ifndef CLASS_CUI_H_
+#define CLASS_CUI_H_
 
-class CUI {
+class CUI : public CObject{
+
+	CImageRenderer _HPBar;
+	CImageRenderer _HPBarBG;
+
 public:
-	CUI();
-	virtual ~CUI();
+	virtual void Init(SDL_Surface * screen);
+	virtual void Update(int dt);
+	virtual void Render(int dt);
+	virtual void Exit();
 };
 
-#endif /* 2018_1_OSSP_YANGSONSHINPARK_6_SHIN_CLASS_CUI_H_ */
+#endif /* CLASS_CUI_H_ */
